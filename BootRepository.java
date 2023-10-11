a7cc85fe6a705f3904e6a48f36987e7fb5dc711f
LilyTheDev <88348716+MadamHippo@users.noreply.github.com>
2023-10-11 12:54:44 -0700

    Update BootRepository.java

--- BootRepository.java
+++ BootRepository.java
@@ -7,20 +7,43 @@ import org.springframework.data.repository.CrudRepository;
 import com.codecademy.boots.entities.Boot;
 import com.codecademy.boots.enums.BootType;
 
-public interface BootRepository extends CrudRepository<Boot, Integer>{
-  public List<Boot> findBySize(Float size);
-  public List<Boot> findByMaterial(String material);
-  public List<Boot> findByType(BootType type);
-  public List<Boot> findByQuantityGreaterThan(Integer minQuantity);
-  public List<Boot> findByMaterialsAndSize(String material, Float size);
-  public List<Boot> findByMaterialAndQuantityGreaterThan(String material, Integer minQuantity);
-  public List<Boot> findByMaterialAndType(String material, BootType type);
-  public List<Boot> findByMaterialAndSizeAndType(String material, Float size, BootType type);
-  public List<Boot>findByMaterialAndSizeAndTypeAndQuantityGreaterThan(String mataerial, Float size, BootType type, Integer minQuantity);
-  public List<Boot> findByTypeAndSize(BootType type, Float size);
-  public List<Boot> findByTypeAndSizeAndQuantityGreaterThan(BootType type, Float size, Integer minQuantity);
-  public List<Boot> findByTypeAndQuantityGreaterThan(BootType type, Integer minQuantity);
-  public List<Boot> findBySizeAndQuantityGreaterThan(Float size, Integer minQuantity);
+public interface BootRepository extends CrudRepository<Boot, Integer> {
+    // Find boots by size
+    public List<Boot> findBySize(Float size);
 
-  
+    // Find boots by material
+    public List<Boot> findByMaterial(String material);
+
+    // Find boots by type
+    public List<Boot> findByType(BootType type);
+
+    // Find boots with quantity greater than a specified minimum
+    public List<Boot> findByQuantityGreaterThan(Integer minQuantity);
+
+    // Find boots by both material and size
+    public List<Boot> findByMaterialsAndSize(String material, Float size);
+
+    // Find boots by material and with quantity greater than a specified minimum
+    public List<Boot> findByMaterialAndQuantityGreaterThan(String material, Integer minQuantity);
+
+    // Find boots by material and type
+    public List<Boot> findByMaterialAndType(String material, BootType type);
+
+    // Find boots by material, size, and type
+    public List<Boot> findByMaterialAndSizeAndType(String material, Float size, BootType type);
+
+    // Find boots by material, size, type, and quantity greater than a specified minimum
+    public List<Boot> findByMaterialAndSizeAndTypeAndQuantityGreaterThan(String material, Float size, BootType type, Integer minQuantity);
+
+    // Find boots by type and size
+    public List<Boot> findByTypeAndSize(BootType type, Float size);
+
+    // Find boots by type, size, and quantity greater than a specified minimum
+    public List<Boot> findByTypeAndSizeAndQuantityGreaterThan(BootType type, Float size, Integer minQuantity);
+
+    // Find boots by type and with quantity greater than a specified minimum
+    public List<Boot> findByTypeAndQuantityGreaterThan(BootType type, Integer minQuantity);
+
+    // Find boots by size and with quantity greater than a specified minimum
+    public List<Boot> findBySizeAndQuantityGreaterThan(Float size, Integer minQuantity);
 }
